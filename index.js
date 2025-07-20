4e7139ac4703c38080e9bcefd3e2c66e0fe6db65
lohhick <decossaux.loic@gmail.com>
2025-07-20 15:05:05 +0200

    added script tag, added function in js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function row(pawns){
-    
+function addRow(){
+    game = document.getElementById("game");
+    newRow = document.createElement("div");
+    newRow.className = "pions";
+    game.appendChild(newRow);
 }
+addRow()
