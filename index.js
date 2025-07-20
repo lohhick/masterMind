3af90199c7e86fda36a46b6c611e3d6bc3bc7847
ilouche2 <ilias.jaghadal@gmail.com>
2025-07-20 16:03:05 +0200

    added for loop in js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function addRow(){
+function addRow(colors){
     game = document.getElementById("game");
     newRow = document.createElement("div");
     newRow.className = "pions";
+    for (let i in colors){
+        console.log(i)
+        pawn = document.createElement("div")
+        pawn.textContent = "__"
+        pawn.style.backgroundColor = colors[i]
+        newRow.appendChild(pawn)
+    }
     game.appendChild(newRow);
 }
-addRow()
+addRow(["aqua","blueviolet","red","black"])
